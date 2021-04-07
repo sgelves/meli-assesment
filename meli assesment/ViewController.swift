6efd6b66f70f53667a7507ba43cb52231b4624e1
Sergio Gelves <sergiogelvesmendoza@gmail.com>
2021-04-07 14:42:55 -0500

    Initial Commit

---
+++ meli assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meli assesment
+//
+//  Created by Sergio Gelves on 7/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
